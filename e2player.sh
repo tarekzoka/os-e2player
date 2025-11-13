c3cebec6662c5ae451bdf279e48157659720e561
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-11-13 11:37:56 +0200

    Add files via upload

--- e2player.sh
+++ e2player.sh
@@ -1,70 +1,50 @@
-#!/bin/bash
-#
-##setup command=wget https://raw.githubusercontent.com/tarekzoka/os-e2player/main/e2player.sh-O - | /bin/sh
-#
-echo "==============================================="
-echo "       Download And Install Plugin e2player     "
-echo "==============================================="
+#!/bin/sh
 
-TEMPATH="/tmp"
-    dpkg -i --force-overwrite $MY_DEB
+#wget https://raw.githubusercontent.com/tarekzoka/os-e2player/main/e2player.sh-O - | /bin/sh
+
+##########################################
+version=000
+#############################################################
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-reinstall"
+MY_IPK="enigma2-plugin-extensions-iptvplayer_v1.0_all.ipk"
 MY_DEB="enigma2-plugin-extensions-iptvplayer_v1.0_all.deb"
-MY_URL="https://raw.githubusercontent.com/tarekzoka/os-e2player/main" 
-# Detect system type
-if [ -f /etc/apt/apt.conf ]; then
-    STATUS='/var/lib/dpkg/status'
-    OS='DreamOS'
-elif [ -f /etc/opkg/opkg.conf ]; then
-    STATUS='/var/lib/opkg/status'
-    OS='Opensource'
-else
-    echo "❌ Unknown OS type"
-    exit 1
-fi
+MY_URL="https://gitlab.com/hanfy1971/plugin/-/raw/main/os-e2player"
+# remove old version #
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
 
-echo "==> Removing old version..."
-opkg remove enigma2-plugin-extensions-estalker > /dev/null 2>&1
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/EStalker
 echo ""
-
-cd $TEMPATH || exit 1
+# Download and install plugin
+cd /tmp
 set -e
+     wget "$MY_URL/$MY_IPK"
+  wait
+     wget "$MY_URL/$MY_DEB"
 
-# Download and install the correct package
-if which dpkg > /dev/null 2>&1; then
-    wget -q "$MY_URL/$MY_DEB"
-    dpkg -i --force-overwrite $MY_DEB
-    apt-get install -f -y
-    rm -f $MY_DEB
-else
-    wget -q "$MY_URL/$MY_IPK"
-    $OPKGINSTALL $MY_IPK
-    rm -f $MY_IPK
-fi
-
-echo "==============================================="
-echo "✅ Plugin estalker installed successfully"
-echo "==============================================="
+ if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
+	else
+		$OPKGINSTALL $MY_IPK
+	fi
+echo "================================="
 set +e
-
-echo ""
-echo "************************************************"
-echo "   UPLOADED BY  >>>>   TAREK_HANFY              "
-echo "************************************************"
-sleep 4
-
-echo ""
-echo "################################################"
-echo "#         Restarting Enigma2 GUI              #"
-echo "################################################"
-sleep 2
-
-if [ "$OS" = "DreamOS" ]; then
-    systemctl restart enigma2
-else
-    killall -9 enigma2
+cd ..
+wait
+rm -f /tmp/*.ipk
+rm -f /tmp/*.deb
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
 fi
-
+		echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   TARK_HANFY "   
+sleep 4;
+		echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
 exit 0
+
+
+
 
 
